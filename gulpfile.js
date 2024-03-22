93e1b7d6fd3d6325d5c75a88e240fa0bcd3dce81
Sergio Hidalgo <shidalgoc@auna.pe>
2024-03-22 16:52:08 -0500

    desploy

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const uglify = require("gulp-uglify-es").default;
 const { src, dest } = require("gulp");
 
 const minify = () =>
-  src("./cache/src/**/*.js").pipe(uglify()).pipe(dest("./dist"));
+  src("./cache/src/**/*.js")
+    .pipe(uglify({ output: { comments: "preserve" } }))
+    .pipe(dest("./dist"));
 
 exports.minify = minify;
